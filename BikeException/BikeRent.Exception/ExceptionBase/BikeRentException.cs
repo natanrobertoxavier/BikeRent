2f33d91d09ed193e122cdeed8ba04a1eb689e9c8
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2025-08-18 11:04:43 -0300

    feat: adds project structure

---
+++ BikeException/BikeRent.Exception/ExceptionBase/BikeRentException.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace BikeRent.Exception.ExceptionBase;
+public class BikeRentException : SystemException
+{
+    public BikeRentException(string mensagem) : base(mensagem)
+    {
+    }
+
+    protected BikeRentException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ BikeException/BikeRent.Exception/ExceptionBase/InvalidLoginException.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace BikeRent.Exception.ExceptionBase;
+
+[Serializable]
+public class InvalidLoginException : BikeRentException
+{
+    public InvalidLoginException() : base(ErrorsMessages.InvalidLogin)
+    {
+    }
+
+    protected InvalidLoginException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
